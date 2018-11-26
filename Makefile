8aa79d4e448cddf852b808204685cc66928fa37b
netuoso <netuoso@pobox.com>
2018-11-26 15:14:48 -0600

    dynamic app creator in makefile

--- Makefile
+++ Makefile
@@ -27,6 +27,7 @@ APPVERSION_P=0
 APPVERSION=$(APPVERSION_M).$(APPVERSION_N).$(APPVERSION_P)
 
 APPNAME = Steem
+APPCREATOR = netuoso
 APP_LOAD_PARAMS += --appFlags 0x40 --path "44'/135'" --curve secp256k1 $(COMMON_LOAD_PARAMS) --delete 
 
 #prepare hsm generation
--- src/main.c
+++ src/main.c
@@ -151,7 +151,7 @@ const ux_menu_entry_t menu_settings[] = {
 
 const ux_menu_entry_t menu_about[] = {
     {NULL, NULL, 0, NULL, "Version", APPVERSION, 0, 0},
-    {NULL, NULL, 1, NULL, "Created by", "netuoso", 0, 0},
+    {NULL, NULL, 1, NULL, "Created by", APPCREATOR, 0, 0},
     {menu_main, NULL, 2, &C_icon_back, "Back", NULL, 61, 40},
     UX_MENU_END};
 
